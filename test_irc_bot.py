66feb252686c7b5c13ab190bec382db259b3ff55
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-07-05 12:57:28 +0100

    slow, randomise

--- test_irc_bot.py
+++ test_irc_bot.py
@@ -13,7 +13,7 @@ words = """area book business case child company country day eye fact family gov
 lot man money month mother night number part people place point problem program question right room school
 state story student study system thing time water way week woman word work world year""".split()
 
-nick = "".join([random.choice(letters) for n in range(8)])
+nick = "".join([random.choice(letters) for n in range(random.randint(5,8))])
 
 s = socket.socket()
 s.connect((serveraddr, serverport))
@@ -26,8 +26,8 @@ s.send("JOIN #test\n".encode("ascii"))
 last_msg_time = time.time();
 
 while True:
-	if time.time() - last_msg_time > 1:
-		msg = " ".join([random.choice(words) for n in range(7)]) + random.choice([".", "?", "!"]);
+	if time.time() - last_msg_time > 5:
+		msg = " ".join([random.choice(words) for n in range(random.randint(3,8))]) + random.choice([".", "?", "!"]);
 		s.send("PRIVMSG #test :{}\n".format(msg).encode("ascii"))
 		last_msg_time = time.time();
 	try:
